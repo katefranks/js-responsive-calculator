f6172c37db4341623b34b733e20a537d5b11c3d1
katefranks <kmfranks9@gmail.com>
2021-08-12 17:12:58 -0400

    removed alerts for buttons, updating calc screen instead

--- scripts/main.js
+++ scripts/main.js
@@ -30,48 +30,47 @@ $plusMinus.addEventListener('click', pushPlusMinus);
 $percent.addEventListener('click', pushPercent);
 $decimal.addEventListener('click', pushDecimal);
 
-
-
 // pushnumber alerts the number associated with its event argument when called
 // value vs text content- text content is the human readable version. Typically target value, that way you can change the user's visual w/o having to go back and refactor something else.
 
 function pushNumber(event) {
-  // alert(event.target.value); //target is the actual element
   calculation.push(event.target.value);
   $calcTotal.value = event.target.value;
 }
 
 
 function pushOperator(event) {
-  alert(event.target.value);
   calculation.push(event.target.value);
-
   total = '';
   $calcTotal.value = event.target.value;
 }
 
 function pushClear(event) {
-  alert('Screen Cleared');
+  // alert('Screen Cleared');
   calculation = [];
   total = '';
+  $calcTotal.value = '0';
 }
 
 function pushPlusMinus(event) {
-  alert(event.target.value);
+  // alert(event.target.value);
   calculation.push(event.target.value);
   console.log(calculation);
+  $calcTotal.value = event.target.value;
 }
 
 function pushPercent(event) {
-  alert(event.target.value);
+  // alert(event.target.value);
   calculation.push(event.target.value);
   console.log(calculation);
+  $calcTotal.value = event.target.value;
 }
 
 function pushDecimal(event) {
-  alert(event.target.value);
+  // alert(event.target.value);
   calculation.push(event.target.value);
   console.log(calculation);
+  $calcTotal.value = event.target.value;
 }
 
 
@@ -79,7 +78,7 @@ function pushDecimal(event) {
 // for loop, giving back num1, num2, and the operator.
 
 function calculate() {
-  alert('=');
+  // alert('=');
 
   calculation.push(parseInt(total));
   calculation.push(event.target.value);
@@ -109,224 +108,13 @@ function calculate() {
 
   //operator is still a string, no way to convert that, so explaining what to return.
   if (operator === '+') {
-    // return num1 + num2;
-    alert(num1 + num2);
     totalValue = num1 + num2;
   } else if (operator === '-') {
-    // return num1 - num2;
-    alert(num1 - num2);
-    console.log(num1 - num2);
     totalValue = num1 - num2;
   } else if (operator === '*') {
-    // return num1 * num2;
-    alert(num1 * num2);
-    console.log(num1 * num2);
     totalValue = num1 * num2;
   } else if (operator === '/') {
-    // return num1 / num2;
-    alert(num1 / num2);
-    console.log(num1 / num2);
     totalValue = num1 / num2;
   }
     $calcTotal.value = totalValue;
 }
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////
-////////  Section below is previous attempts & notes   ///////////
-/////////////////////////////////////////////////////////////////
-
-// var calculation = [];
-//
-// // for (var i=0; i < calculation.length; i++) {
-// //   parseInt(calculation[i]);
-// // }//
-// //////// changed below from textConten to Value in above.
-// function pushNumber(event) {
-//   alert(event.target.textContent);
-// }
-//
-//
-// const $numberButtons = document.querySelectorAll(".number");
-// // console.log($numberButtons);
-// $numberButtons.forEach(function(number) {
-//   // console.log(number);
-//   number.addEventListener('click', function pushNumber(){
-//     calculation.push(parseFloat(this.value));
-//     console.log(calculation);
-//
-// })
-// });
-//
-// // ********************
-// // Create push operator function
-// // (`+`, `-`, `*`, `/`, `C`)
-//
-// // *****trying this set up instead*****
-// // function pushOperator(event) {
-// //   alert(event.target.value);
-// //   calculation.push(parseInt(number));
-// //   calculation.push(event.target.value);
-// //   number = '';
-// // ******original code******
-// function pushOperator(event) {
-//   alert(event.target.textContent);
-// }
-// // multiple selectors- separate w/ commas
-// const $operatorButtons = document.querySelectorAll(".operator, .clear");
-// // console.log($operatorButtons);
-// $operatorButtons.forEach(function(operator) {
-//
-//   operator.addEventListener('click', function pushOperator(){
-//     calculation.push(this.value);
-//     console.log(calculation);
-//   });
-//
-// });
-//
-//
-//
-//
-// //**step 3 function calculate =  class="equal-sign"
-// // ****may need to come back to this- original code.
-// function pushCalculate(event) {
-//   alert(event.target.textContent);
-// }
-//
-// const $calculateButton = document.querySelectorAll(".equal-sign");
-// // console.log($calculateButton);
-// $calculateButton.forEach(function(calculate) {
-//
-//   calculate.addEventListener('click', function pushCalculate(){
-//     calculation.push(this.value);
-//     console.log(calculation);
-//   });
-// });
-//
-// // // multiple selectors- separate w/ commas
-//
-// // calculation =
-// // for (var i=0; i < calculation.length; i++) {
-// //   parseFloat(calculation[i]);
-// //   console.log(calculation);
-// // }
-//
-//
-//
-// var total;
-// var num1 = calculation[0];
-// var num2 = calculation[2];
-//
-// for (let i = 0; i < calculation.length; i++)
-// if ($operatorButtons.innerHTML == '+') {
-//     total = num1 + num2;
-//     console.log(total);
-//
-// }
-// else if ($operatorButtons.innerHTML == '-') {
-//     total = num1 - num2;
-//     console.log(total);
-// }
-// else if ($operatorButtons.innerHTML == '*') {
-//     total = num1 * num2;
-//     console.log(total);
-// }
-// else {
-//     total = num1 / num2;
-//     console.log(total);
-// }
-//
-// ////// NOTES ///////
-//
-// // **step 4: define a variable 'calculation' pointing to empty array
-//
-//
-// // calculation = calculation.push(this.value);
-//
-// // calculation.push(event.target.textContent);
-// // (this.value)
-// // **step 5: Using `Array.prototype.push`, perform actions on the `calculation` variable
-// // when numbers and operators are pressed
-//
-//
-//
-// // $calculateButton.forEach(function(equals) {
-// //
-// //   equals.addEventListener('click', calculate);
-// //
-// // });
-//
-// // function pushNumber(event) {
-// //   alert(event.target.textContent);
-// // }
-// //
-// //
-// // const $numberButtons = document.querySelectorAll(".number");
-// // console.log($numberButtons);
-// // $numberButtons.forEach(function(number) {
-// //   // console.log(number);
-// //   number.addEventListener('click', pushNumber);
-// // //
-// // });
-//
-//
-//
-// // function demo(params) {
-// //   console.log(params);
-// // }
-// //
-// // demo('Hey');
-// // demo('Hello');
-// //
-//
-//   // for(let i=0; i < $numberButtons.length; i+=) {
-//   //     console.log($numberButtons[i]);
-//   // }
-//
-//
-//
-// // forEach($numberButtons);
-// //   console.log($numberButtons);
-// //
-//
-//
-// // var myNodeList = document.querySelectorAll('li');
-// // forEach($numberButtons, function (index, value) {
-// //   console.log(index, value); // passes index + value back!
-// // });
-//
-// // for(var i = 0; i < $numberButtons.length; i ++) {
-// //   console.log($numberButtons);
-// // }
-//
-//
-//
-// // console.log('button', $button);
-// // $button.addEventListener('click', pushNumber);
-//
-// // node list for each
-//
-//
-// // ***no alerts
-// // const $button = document.querySelectorAll(".number");
-// // $button.addEventListener('click', pushNumber);
-//
-// // ***only 7 alerts
-// // const $button = document.querySelector(".number");
-// // $button.addEventListener('click', pushNumber);
-//
-//
-//
-//
-//
-// // event.target.textContent.
-//
-// // const $button = document.querySelectorAll('button');  **target just the # buttons
-//
-// // const $button = document.querySelectorAll('button');
-// // $button.addEventListener('click', pushNumber);
